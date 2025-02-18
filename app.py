605549df29f05119f39aaf5e14cecd512a237006
Luis Martinez <lcmartinezl05@gmail.com>
2025-02-18 15:04:04 -0500

    Routes and Views

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hola mundo!"
+
+@app.route("/acerca-de")
+def about():
+    return "Esto es una app de notas"
+
+@app.route("/contacto", methods=['GET', 'POST'])
+def contact():
+    if request.method == "POST":
+        return "Formulario enviado correctamente", 201
+    return "Pagina de contacto"
+
+
+@app.route("/api/info")
+def api_info():
+    data = {
+        "nombre": "Notes App",
+        "version": "1.1.1"
+    }
+    return jsonify(data), 200
