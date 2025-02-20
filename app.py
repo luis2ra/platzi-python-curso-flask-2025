034bc66f205a981eba276ede6b6f7dd44cff0990
Luis Martinez <lcmartinezl05@gmail.com>
2025-02-20 16:10:47 -0500

    auth blueprint

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask, request
 from config import Config
 from models import db
 from notes.routes import notes_bp
+from auth.routes import auth_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 app.register_blueprint(notes_bp)
+app.register_blueprint(auth_bp)
 
 
 @app.route("/acerca-de")
