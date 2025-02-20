2784e61d05c7d399635fe7eff03075e4a86cd9f8
Luis Martinez <lcmartinezl05@gmail.com>
2025-02-20 15:50:47 -0500

    Jinja templates + tailwind css

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now())
 
     def __repr__(self):
         return f"<Note {self.id}: {self.title}>"
